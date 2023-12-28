9d4912d5694c3385e91fc6f047fb58b1748e6a86
Tariq Zaitoun <tariq.s.zaitoun@gmail.com>
2023-12-28 18:22:43 -0500

    Added Registration Controller with CRUD operations and tested.

---
+++ src/main/java/com/tariq/springboot/tickets/registration/RegistrationController.java
@@ -0,0 +1,36 @@
+package com.tariq.springboot.tickets.registration;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.NoSuchElementException;
+
+@RestController
+@RequestMapping(path = "/registrations")
+public class RegistrationController {
+    private final RegistrationRepository registrationRepository;
+
+    public RegistrationController(RegistrationRepository registrationRepository) {
+        this.registrationRepository = registrationRepository;
+    }
+
+    @PostMapping
+    public Registration create(@RequestBody Registration registration) { // @RequestBody will make the MVC parse the body of the http request as JSON and convert it into a Registration object
+        return registrationRepository.create(registration);
+    }
+
+    @GetMapping(path = "/{ticketCode}")
+    public Registration get(@PathVariable("ticketCode") String ticketCode) {
+        return registrationRepository.findByTicketCode(ticketCode)
+                .orElseThrow(() -> new NoSuchElementException("Registration with ticket code" + ticketCode + " not found"));
+    }
+
+    @PutMapping
+    public Registration update(@RequestBody Registration registration) {
+        return registrationRepository.update(registration);
+    }
+
+    @DeleteMapping(path = "/{ticketCode}")
+    public void delete (@PathVariable("ticketCode") String ticketCode) {
+        registrationRepository.deleteByTicketCode(ticketCode);
+    }
+}
